c26fbc65e7461a606fb661733b5753a1c36dc0a4
rajat <rajatrdf@gmail.com>
2026-01-04 16:42:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let aqi = 421;
+let aqi = 221;
 const aqiValue = document.getElementById("aqiValue");
 
 setInterval(() => {
@@ -6,20 +6,16 @@ setInterval(() => {
   aqiValue.textContent = aqi;
 }, 3000);
 
-
+// Water TDS
 let tds = 680;
-
 const tdsValue = document.getElementById("tdsValue");
 const tdsStatus = document.getElementById("tdsStatus");
 
 setInterval(() => {
-  const change = Math.floor(Math.random() * 20) - 10;
-  tds += change;
-
-  // realistic TDS range
+  tds += Math.floor(Math.random() * 20) - 10;
   tds = Math.max(50, Math.min(2000, tds));
 
-  tdsValue.textContent = tds;
+  tdsValue.innerHTML = `${tds} <small>ppm</small>`;
 
   let status = "Unfit";
   if (tds <= 300) status = "Excellent";
@@ -27,5 +23,4 @@ setInterval(() => {
   else if (tds <= 900) status = "Poor";
 
   tdsStatus.textContent = status;
-
 }, 4000);
